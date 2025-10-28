1db57ffc47f1bcf529be99de3d7ce9190ecc0afe
Hyunsuk Ju <wngustjr1306@naver.com>
2025-10-28 22:41:07 +0900

    first commit

---
+++ src/test/java/com/ucamp/coffee/CoffeeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucamp.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
