5b15ebe27d88b7afbdf11667cf24e47b9f35a787
brobro332 <332bro@naver.com>
2025-11-01 23:42:01 +0900

    refactor: 코드 들여쓰기 및 리팩터링

--- src/main/java/com/ucamp/coffee/domain/subscription/repository/MemberSubscriptionRepository.java
+++ src/main/java/com/ucamp/coffee/domain/subscription/repository/MemberSubscriptionRepository.java
@@ -25,35 +25,35 @@ public interface MemberSubscriptionRepository extends JpaRepository<MemberSubscr
     List<MemberSubscription> findAllByMemberWithRelations(@Param("member") Member member);
 
     @Query("""
-    SELECT o.store.partnerStoreId, COUNT(ms)
-    FROM MemberSubscription ms
-    JOIN Orders o ON o.memberSubscription = ms
-    WHERE o.store.partnerStoreId IN :storeIds
-    GROUP BY o.store.partnerStoreId
-""")
+        SELECT o.store.partnerStoreId, COUNT(ms)
+        FROM MemberSubscription ms
+        JOIN Orders o ON o.memberSubscription = ms
+        WHERE o.store.partnerStoreId IN :storeIds
+        GROUP BY o.store.partnerStoreId
+    """)
     List<Object[]> countSubscribersByStoreIds(@Param("storeIds") List<Long> storeIds);
 
 	//구매 ID로 보유 구독권 찾기
 	Optional<MemberSubscription> findByPurchase_PurchaseId(Long purchaseId);
 
     @Query("""
-    SELECT o.store.partnerStoreId, SUM(ms.dailyRemainCount)
-    FROM MemberSubscription ms
-    JOIN Orders o ON o.memberSubscription = ms
-    WHERE o.store.partnerStoreId IN :storeIds
-    GROUP BY o.store.partnerStoreId
-""")
+        SELECT o.store.partnerStoreId, SUM(ms.dailyRemainCount)
+        FROM MemberSubscription ms
+        JOIN Orders o ON o.memberSubscription = ms
+        WHERE o.store.partnerStoreId IN :storeIds
+        GROUP BY o.store.partnerStoreId
+    """)
     List<Object[]> getRemainingStockByStoreIds(@Param("storeIds") List<Long> storeIds);
 
     @Query("""
-    SELECT o.store.partnerStoreId, 
-           CASE WHEN COUNT(ms) > 0 THEN true ELSE false END
-    FROM MemberSubscription ms
-    JOIN Orders o ON o.memberSubscription = ms
-    WHERE o.store.partnerStoreId IN :storeIds
-    AND ms.member.memberId = :memberId
-    GROUP BY o.store.partnerStoreId
-""")
+        SELECT o.store.partnerStoreId,
+               CASE WHEN COUNT(ms) > 0 THEN true ELSE false END
+        FROM MemberSubscription ms
+        JOIN Orders o ON o.memberSubscription = ms
+        WHERE o.store.partnerStoreId IN :storeIds
+        AND ms.member.memberId = :memberId
+        GROUP BY o.store.partnerStoreId
+    """)
     List<Object[]> isSubscribedByMemberAndStoreIds(@Param("storeIds") List<Long> storeIds,
                                                    @Param("memberId") Long memberId);
 }
