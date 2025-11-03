a94e6dfc04a8516f1692773bcc1c7e6e32326257
brobro332 <332bro@naver.com>
2025-11-03 17:13:57 +0900

    setting: jwtFilter에 화이트리스트 추가 및 jjwt 의존성 다운그레이드

--- src/main/java/com/ucamp/coffee/common/security/JwtFilter.java
+++ src/main/java/com/ucamp/coffee/common/security/JwtFilter.java
@@ -24,6 +24,13 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.startsWith("/oauth")
+                || path.startsWith("/api/auth");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = null;
 
